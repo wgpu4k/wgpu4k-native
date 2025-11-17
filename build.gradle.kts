6571fb07c09ebbbc2e4e6ce0b6f966534cb5368f
ygdrasil-io <alexandre.mommers@gmail.com>
2025-11-17 14:59:48 +0100

    Bump project version to `v27.0.4-SNAPSHOT`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ allprojects {
 	}
 
 	group = "io.ygdrasil"
-	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "v27.0.2-SNAPSHOT"
+	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "v27.0.4-SNAPSHOT"
 }
 
 
